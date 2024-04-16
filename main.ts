69029ee420a67178594804870bc9bfc951e50625
Zohaib Javed <zohaibjaved@gmail.com>
2024-04-16 10:45:34 +0500

    Questions 136, 137 and 138

---
+++ main.ts
@@ -0,0 +1,26 @@
+// Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+
+for (let i = 1; i <= 5; i++) {
+    console.log(`Iteration ${i}, i value:, i`);
+}
+
+console.log("\n");
+
+// Question 137: Implement a try-catch block to handle potential errors in a block of code.
+
+try {
+    throw new Error("Something went wrong");
+} catch (error) {
+    console.log(error.message);
+}
+
+console.log("\n");
+
+// Question 138: Describe how to use breakpoints in browser developer tools to debug JavaScript code.
+
+console.log("Before breakpoint");
+
+console.log(
+    "This line has a breakpoint set on it in the browser's developer tools"
+);
+console.log("After breakpoint");
